c60905feea3003bf66cd3f34ea0dfb576e1db3af
Brandon Wever <brandonwever@gmail.com>
2026-03-05 15:13:33 -0500

    github pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bwever.github.io',
+  base: '/tomlify',
   integrations: [react()]
 });
